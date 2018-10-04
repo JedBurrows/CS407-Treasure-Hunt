08dfcac8bb66a4234f0e970989230484a4b42198
Jed Burrows <jed@Jeds-MacBook-Pro.local>
2018-10-04 12:43:18 +0100

    added output to a txt

--- src/ImageDecoder.java
+++ src/ImageDecoder.java
@@ -1,15 +1,14 @@
 import javax.imageio.ImageIO;
+import javax.xml.crypto.Data;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.sql.SQLOutput;
 import java.util.Arrays;
 
 public class ImageDecoder {
     private BufferedImage cipherImg;
-    private BufferedImage originalImg
-            ;    public ImageDecoder() throws IOException {
+    public ImageDecoder() throws IOException {
         cipherImg = ImageIO.read(new File("res/stego_cover14.bmp"));
-        //System.out.println(Integer.toBinaryString(cipherImg.getRGB(0,0)));
     }
 
     public void DecodeBPCS(){
@@ -27,32 +26,51 @@ public class ImageDecoder {
         outer:
         for (int y = 0; y < cipherImg.getHeight(); y++) {
             for (int x = 0; x < cipherImg.getWidth(); x++) {
-                if(res.length() > 83 * 8){
-                    break outer;
-                }
                 String bp = Integer.toBinaryString(cipherImg.getRGB(x, y)).substring(8);
                 String red = bp.substring(0,8);
                 String green = bp.substring(8,16);
                 String blue = bp.substring(16,24);
 
+                // add LSB to result
+                if(res.length() > 83 * 8){
+                    break outer;
+                }
                 res += red.charAt(red.length()-1);
+                if(res.length() > 83 * 8){
+                    break outer;
+                }
                 res += green.charAt(green.length()-1);
+                if(res.length() > 83 * 8){
+                    break outer;
+                }
                 res += blue.charAt(blue.length()-1);
 
             }
         }
         char[] characters = new char[res.length()/8];
-        res = new StringBuilder(res).reverse().toString();
         for(int i=0, j=0; i<res.length(); i++){
             if(i % 8 == 0 && i>0){
-                characters[j] = (char) Integer.parseInt(res.substring(i - 8, i));
+                characters[j] = (char) (Integer.parseInt(res.substring(i - 8, i)));
                 j++;
             }
         }
+        String fname = "output.txt";
+        try{
+            FileOutputStream fos = new FileOutputStream(fname);
+            DataOutputStream dos = new DataOutputStream(fos);
 
+            for(char c : characters){
+                dos.writeChar(c);
 
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println(e);
+        }
+        catch (IOException e){
+            System.out.println(e);
+        }
 
-        System.out.println("Result: " + String.valueOf(characters));
 
     }
 }
